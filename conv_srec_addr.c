7c73394208ee59f52f0b173bbb43260d0686ed7e
Yohei Endo <yoheie@gmail.com>
2022-11-18 17:31:06 +0900

    Update copyright year.

--- conv_srec_addr.c
+++ conv_srec_addr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Yohei Endo <yoheie@gmail.com>
+ * Copyright (c) 2021-2022 Yohei Endo <yoheie@gmail.com>
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
